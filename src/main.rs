f55d040a926f09d5e31080bfe7743a2bb451ac91
gauthier13 <gauthierleclair1@outlook.fr>
2025-11-12 21:54:02 +0100

    implement call to page_title

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-fn main() {
-    prntln!("Hello, world!");
-}
-
 use trpl::Html;
 
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    })
+}
+
+/**
+Return the first title in the document if there is one
+*/
 async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
+    let response_text = trpl::get(url).await.text().await;
     Html::parse(&response_text)
         .select_first("title")
         .map(|title| title.inner_html())
